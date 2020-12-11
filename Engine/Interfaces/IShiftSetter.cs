bc6d3d4810c28a0daa122bc8ce8a3f700dfd886e
Gosha <djabarov.george@gmail.com>
2020-12-11 17:29:34 +0200

    first commit

---
+++ Engine/Interfaces/IShiftSetter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Engine.Interfaces
+{
+    protected interface IShiftSetter
+    {
+        void SetNumberOfShiftsInWeek();
+        void SetNumberOfDaysOfWork();
+        void SetConstraints();
+    }
+}
