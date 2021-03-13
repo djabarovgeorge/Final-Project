e8f957a4d49f488eb03173f6e2c62ba9b1eda828
nir vaknin <nirviko@gmail.com>
2021-03-13 16:55:06 +0200

    greedy algo conflicts

--- Engine/Algorithms/GreedyAlgorithm.cs
+++ Engine/Algorithms/GreedyAlgorithm.cs
@@ -1,4 +1,5 @@
-﻿using Engine.Models;
+﻿using Engine.Extensions;
+using Engine.Models;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -6,6 +7,9 @@ namespace Engine.Algorithms
 {
     public class GreedyAlgorithm
     {
+
+        private List<string> week = new List<string> { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+        private List<string> shift = new List<string> { "Morning", "Noon", "Afternoon", "Night" };
 
         public void Execute(Schedulare schedulare, ShiftsContainer shiftsContainer)
         {
@@ -18,7 +22,7 @@ namespace Engine.Algorithms
 
         public void StartAlgorithm(Schedulare schedulare, ShiftsContainer shiftsContainer)
         {
-            List<GreedyEmployee> greedyEmpList = new List<GreedyEmployee>();
+            var greedyEmpList = new List<GreedyEmployee>();
 
             foreach (var emp in shiftsContainer.EmployeeConstraints)
             {
@@ -53,9 +57,39 @@ namespace Engine.Algorithms
                 greedyEmpList.Add(greedyEmploee);
             }
 
-
+                
             var conflicts = greedyEmpList.Where(x => x.ShiftsThatWasNotAssigned.Count > 0).ToList();
+
+            foreach(var employeeCheck in conflicts)
+            {
+                if(employeeCheck.ShiftsThatWasNotAssigned.Count > 1)
+                {
+                    foreach(var iDontHaveThisDayList in employeeCheck.ShiftsThatWasNotAssigned)
+                    {
+                        var shiftDay = schedulare.Days.FirstOrDefault(x => x.Name.ContainsContent(iDontHaveThisDayList.Day));
+                        var shift = shiftDay.Shifts.FirstOrDefault(x => x.Name.ContainsContent(iDontHaveThisDayList.ShiftName));
+                        //schedulare.Days[week.IndexOf(iDontHaveThisDay.Day)];
+                        var currShiftGreedyWorkers = shift.Workers.Select(x => greedyEmpList.FirstOrDefault(y => x.Name.ContainsContent(y.Name))).ToList();
+
+                        var luckyWorks = currShiftGreedyWorkers.OrderBy(x => x.ShiftsThatWasNotAssigned.Count()).LastOrDefault();
+
+                        //if greater then some value
+
+                        //if the ShiftsThatWasNotAssigned is the same
+
+
+
+                    }
+
+
+                    // if (newEployw.NumberOfShiftsThatWasAssigned == shiftsContainer.EmployeeConstraints[])
+                }
+            }
 
         }
     }
+
+
+
+
 }
