ff6adae50d189a65293b9ca37ee7a40d93617101
Gosha <djabarov.george@gmail.com>
2021-03-13 15:38:09 +0200

    randalgo added ResolveAndFillTheSchdulare

--- Engine/Algorithms/RandAlgorithm.cs
+++ Engine/Algorithms/RandAlgorithm.cs
@@ -1,4 +1,5 @@
-﻿using Engine.Models;
+﻿using Engine.Extensions;
+using Engine.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,8 +21,40 @@ namespace Engine.Algorithms
             ValidateInitialStep(schedulare, shiftsContainer);
 
 
+            // step 3 resolve conflicts from previous step
+            ResolveAndFillTheSchdulare(schedulare, shiftsContainer);
 
         }
+
+        private void ResolveAndFillTheSchdulare(Schedulare schedulare, ShiftsContainer shiftsContainer)
+        {
+            var numberOfWokersInShift = shiftsContainer.ShiftParams.NumberOfWokersInShift;
+
+            while (!_workersBackLog.Count.Equals(0))
+            {
+                // get available shifts
+                var days = schedulare.Days.DeepClone();
+                var tempAvailableShifts = days.Select(x => new { x.Name, Shifts = x.Shifts.Where(y => y.Workers.Count < numberOfWokersInShift).ToList() }).ToList();
+                var availableShifts = tempAvailableShifts.Where(x => !x.Shifts.IsNullOrEmpty()).ToList();
+
+                // fetch shift from back log
+                var backLogShiftDayToFill = availableShifts.FirstOrDefault();
+                var backLogShiftToFill = new { backLogShiftDayToFill.Name, Shift = backLogShiftDayToFill.Shifts.FirstOrDefault() };
+
+                // fetch matching shift from schedulare as was fetched from the backlog
+                var schedulareDay = schedulare.Days.FirstOrDefault(x => x.Name.ContainsContent(backLogShiftDayToFill.Name));
+                var schedulareShift = schedulareDay.Shifts.FirstOrDefault(x => x.Name.ContainsContent(backLogShiftToFill.Shift.Name));
+
+                var randomEmployee = GetRandomEmployee();
+
+                // add worker to the schedulare 
+                schedulareShift.Workers.Add(randomEmployee);
+
+                // remove worker from the backlog
+                _workersBackLog.Remove(randomEmployee);
+            }
+        }
+
 
         private void ValidateInitialStep(Schedulare schedulare, ShiftsContainer shiftsContainer)
         {
@@ -86,6 +119,14 @@ namespace Engine.Algorithms
             }
             return randomList;
         }
+
+        private Worker GetRandomEmployee()
+        {
+            Random random = new Random();
+            int rInt = random.Next(0, _workersBackLog.Count());
+            return _workersBackLog[rInt];
+        }
+
 
         private static void InitialTheSchedular(Schedulare schedulare, ShiftsContainer shiftsContainer)
         {
