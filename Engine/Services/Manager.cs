8e35a0fc2127e4bebb2e788dca7b587a31b024bb
Gosha <djabarov.george@gmail.com>
2021-04-12 22:30:52 +0300

    removed redundant code

--- Engine/Services/Manager.cs
+++ Engine/Services/Manager.cs
@@ -33,67 +33,9 @@ namespace Engine.Services
             //_bfs.Execute(schedulare, shiftsContainer);
 
             _waStar.Execute(schedulare, shiftsContainer);
-
-
-            double count = 0;
-            double avarage = 0;
-            var max = new { Average = 0.0, WeightContainer = new WeightContainer() };
-
-            for (int TwoShiftsInARowWeight = 10; TwoShiftsInARowWeight < 100; TwoShiftsInARowWeight += 10)
-            {
-                for (int LackSatisfactionConstraintsWeight = 100; LackSatisfactionConstraintsWeight < 1000; LackSatisfactionConstraintsWeight+=10)
-                {
-                    for (int ShiftsInARow = 10; ShiftsInARow < 100; ShiftsInARow+=10)
-                    {
-                        for (int UnwantedShift = 10; UnwantedShift < 100; UnwantedShift+=10)
-                        {
-                            count = 0;
-
-                            var currWheights = new WeightContainer()
-                            {
-                                TwoShiftsInARowWeight = TwoShiftsInARowWeight,
-                                LackSatisfactionConstraintsWeight = LackSatisfactionConstraintsWeight,
-                                ShiftsInARow = ShiftsInARow,
-                                UnwantedShift = UnwantedShift
-                            };
-
-                            Console.WriteLine($"{TwoShiftsInARowWeight} {LackSatisfactionConstraintsWeight} {ShiftsInARow} {UnwantedShift}");
-
-                            var runTimes = 1;
-
-                            for (int i = 0; i < runTimes; i++)
-                            {
-                                shiftsContainer = ManageShifts();
-
-                                schedulare = new Schedulare(shiftsContainer);
-
-                                var resultSchedulare = _bfs.Execute(schedulare, shiftsContainer, currWheights);
-
-                                count = CommonLogic.GetPercentageOfSatisfaction(resultSchedulare, shiftsContainer);
-                            }
-                            avarage = count / runTimes;
-                            Console.WriteLine($"avarage {avarage}");
-                            var localMax = new
-                            {
-                                Average = avarage,
-                                WeightContainer = currWheights
-                            };
-                            max = max.Average > localMax.Average ? max : localMax;
-
-                            Console.WriteLine($"max {max.Average} {max.WeightContainer.TwoShiftsInARowWeight} {max.WeightContainer.LackSatisfactionConstraintsWeight} {max.WeightContainer.ShiftsInARow} {max.WeightContainer.UnwantedShift}");
-                        }
-                    }
-
-                }
-            }
-            _bfs.Execute(schedulare, shiftsContainer);
-
         }
 
-        //public int TwoShiftsInARowWeight { get; set; }
-        //public int LackSatisfactionConstraintsWeight { get; set; }
-        //public int ShiftsInARow { get; set; }
-        //public int UnwantedShift { get; set; }
+
 
         public ShiftsContainer ManageShifts()
         {
@@ -118,13 +60,6 @@ namespace Engine.Services
 
             return shiftContainer;
         }
-
-
-        // use out huristic mathods
-        //...
-
-        // result 
-        //...
     }
 }
 
