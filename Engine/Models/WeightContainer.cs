6aa381f30180d06c38e968d08d40e71e609a9eaf
Gosha <djabarov.george@gmail.com>
2021-04-12 22:17:50 +0300

    added WeightContainer model

---
+++ Engine/Models/WeightContainer.cs
@@ -0,0 +1,10 @@
+﻿namespace Engine.Models
+{
+    public class WeightContainer
+    {
+        public int TwoShiftsInARowWeight { get; set; }
+        public int LackSatisfactionConstraintsWeight { get; set; }
+        public int ShiftsInARow { get; set; }
+        public int UnwantedShift { get; set; }
+    }
+}
