922808b32d22a0a91c08ebc4d8357aafa187bd12
Gosha <djabarov.george@gmail.com>
2021-04-12 22:16:43 +0300

    added SchedulareState model

---
+++ Engine/Models/SchedulareState.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Engine.Models
+{
+    public class SchedulareState
+    {
+        //public Schedulare Schedulare { get; set; }
+        public int Weight { get; set; }
+        public TreeNode<Schedulare> Node { get; set; }
+    }
+}
